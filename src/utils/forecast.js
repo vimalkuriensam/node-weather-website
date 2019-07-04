f770422f8eb5bd662265a209012b196f6cdab40a
vimalkuriensam <vimalkuriensam@gmail.com>
2019-07-04 20:28:05 +0530

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude,longitude,callback)=>{
     }
     else {
       callback(undefined,
-        `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is ${body.currently.precipProbability} % chance of rain`
+        `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. The high today is ${body.daily.data[0].temperatureHigh} with a low of ${body.daily.data[0].temperatureLow}. There is ${body.currently.precipProbability} % chance of rain`
     )}
 })}
 
